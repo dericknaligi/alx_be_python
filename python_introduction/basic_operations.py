65b5d1f4df261b5223bda9afebdd491409a6c34d
derick naligi <chahasid@gmail.com>
2024-12-12 23:50:31 +0300

    commiting fron main

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,6 +4,8 @@ Addition = number1 + number2
 print(Addition)
 Subtraction = number1 - number2
 print(Subtraction)
+Multiplication = number1 * number2
+
 
 # Addition of 10 and 5 is 15
 # Subtraction of 10 and 5 is 5
