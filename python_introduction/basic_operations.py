dae74bb6ce799f823e1988922dea86e9dbd8c696
derick naligi <chahasid@gmail.com>
2024-12-12 23:45:26 +0300

    arithmetic

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,9 @@
 number1 = 10
 number2 = 5
+Addition = number1 + number2
+print(Addition)
+Subtraction = number1 - number2
+print(Subtraction)
 
 # Addition of 10 and 5 is 15
 # Subtraction of 10 and 5 is 5
